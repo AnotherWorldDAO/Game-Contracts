4cd45efaa41f512450428e72ef9021e8e337dd0b
Jackie Chia-Hsun Lee <jackylee0424@gmail.com>
2024-07-30 12:19:02 -0700

    use SKALE RNG

--- contracts/AnotherTreasureS2.sol
+++ contracts/AnotherTreasureS2.sol
@@ -125,13 +125,31 @@ contract AnotherTreasureS2 is ERC1155, ERC1155Burnable, Ownable {
         userTimestamps[userAddress] = block.timestamp;
     }
 
-    function random() internal view returns (uint256) {
-        return
-            uint256(
-                keccak256(
-                    abi.encodePacked(block.timestamp, msg.sender)
-                )
-            );
+    function random() public view returns (uint256) {
+        // use SKALE RNG by default
+        uint256 rand = uint256(sklRNG());
+        if (rand > 0) {
+            return rand;
+        } else {
+            // fallback for NON-SKALE
+            return
+                uint256(
+                    keccak256(abi.encodePacked(block.timestamp, msg.sender))
+                );
+        }
+    }
+
+    // SKALE RNG
+    // https://docs.skale.network/tools/skale-specific/random-number-generator
+    function sklRNG() public view returns (bytes32 addr) {
+        assembly {
+            let freemem := mload(0x40)
+            let start_addr := add(freemem, 0)
+            if iszero(staticcall(gas(), 0x18, 0, 0, start_addr, 32)) {
+                invalid()
+            }
+            addr := mload(freemem)
+        }
     }
 
     function isApprovedForAll(
